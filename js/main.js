bd312661ff11a09865dbbd24a370f27b1d0582a5
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-03-10 01:34:55 +0600

    commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    
+	
+}(jQuery));	
+
+
+function copyToClipboard() {
+  /* Get the text field */
+  var copyText = document.getElementById("referralurl");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+  
+}
